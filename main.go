430ade3d46929a21f6b15e1c0168e4776af04a53
Javier Provecho Fernandez <javiertitan@gmail.com>
2016-02-20 02:40:45 +0100

    Create server

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+const cats = true
+const dogs = true
+
+func main() {
+	r := createRouter()
+
+	r.Run(":8080")
+}
+
+func createRouter() (r *gin.Engine) {
+	r = gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "Hello world!")
+	})
+
+	return
+}
