1e2e15f1f35237a729a61bcbb98d0c82bd966070
Javier Provecho Fernandez <javiertitan@gmail.com>
2016-02-20 02:41:39 +0100

    Add test

---
+++ main_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCatsAndDogs(t *testing.T) {
+	if cats && dogs {
+		t.Error("there are cats with dogs in the scene!")
+	}
+}
+
+func TestCreateRouter(t *testing.T) {
+	router := createRouter()
+	list := router.Routes()
+
+	assert.Len(t, list, 1)
+
+	assertRoutePresent(t, list, gin.RouteInfo{
+		Method: "GET",
+		Path:   "/",
+	})
+}
+
+func assertRoutePresent(t *testing.T, gotRoutes gin.RoutesInfo, wantRoute gin.RouteInfo) {
+	for _, gotRoute := range gotRoutes {
+		if gotRoute.Path == wantRoute.Path && gotRoute.Method == wantRoute.Method {
+			assert.Regexp(t, wantRoute.Path, gotRoute.Path)
+			return
+		}
+	}
+	t.Errorf("route not found: %v", wantRoute)
+}
